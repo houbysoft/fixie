a3135d38a78c704707cd3b58b300335e03c67a1c
Keenon Werling <keenon@stanford.edu>
2013-01-25 21:29:32 -0800

    Incorporated regex thing

--- src/fixTokenizer.cpp
+++ src/fixTokenizer.cpp
@@ -1,12 +1,29 @@
 #include <boost/algorithm/string.hpp>
+#include <boost/xpressive/xpressive.hpp>
+#include <iostream>
 #include "fixTokenizer.h"
 
 #ifdef UNIT_TESTING
 public main(int argc, char** argv) {
-    
-    //Unit test harness
-
-
 }
 #endif
+
+FixieTokenizer::FixieTokenizer(std::string input) {
+
+    std::cout<<input<<std::endl;
+
+    //Prints out a list of the tokens
+
+    boost::xpressive::sregex re = boost::xpressive::sregex::compile("[0-9a-zA-Z]+");
+
+    //Iterate over all the words in the input
+
+    boost::xpressive::sregex_token_iterator begin( input.begin(), input.end(), re ), end;
+
+    //Print out all the words
+    
+    std::ostream_iterator< std::string > out_iter( std::cout, "\n" );
+    std::copy( begin, end, out_iter );
+}
+
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,11 @@
 
 
 int main(int argc, char **argv) {
+    
+    //Unit test harness
+
+    FixieTokenizer *tok = new FixieTokenizer("Hello Motherfucing Regex");
+
+    std::cout << "Working" << std::endl;
 }
 
