24ab86afac4f7ada816f7973dae52fdf8ae2dae3
Keenon Werling <keenon@stanford.edu>
2013-01-26 00:24:49 -0800

    Basic code quality maintenance, adding comments, constants, and generally breaking up dense code

--- src/fixieTokenizer.cpp
+++ src/fixieTokenizer.cpp
@@ -16,46 +16,71 @@ FixieTokenizer::FixieTokenizer() {
  * a vector of tokens with this line number noted in their values.
  */
 void FixieTokenizer::tokenize(int lineNumber, std::string input, std::vector<FixieTokenizer::token> *tokenized) {
+
+    //Gets the chunks of the string
+
     std::vector<std::string> *chunked = chunk(stripComments(input));
+
+    //Drops in the line number with the chunks for error reporting
+
     for (int i = 0; i < chunked->size(); i++) {
         FixieTokenizer::token token;
         token.string = chunked->at(i);
         token.lineNumber = lineNumber;
+
+        //Pushes the result into the tokenized array that gets passed in
+
         tokenized->push_back(token);
     }
 }
+
+#define NO_COMMENTS_IN_PROGRESS 0
+#define WAITING_FOR_NEWLINE 1
+#define INSIDE_MULTILINE_COMMENT 2
 
 /*
  * Clears out all the comments from a code input
  */
 std::string FixieTokenizer::stripComments(std::string input) {
-    int mode = 0;
-    // can be either 0 (no comments in progress), 1 (inside // comment, waiting for newline), 2 (inside /* */ comment, wait for */)
+    int mode = NO_COMMENTS_IN_PROGRESS;
+
+    // Simple finite state machine for clearing out comments
 
     std::string res;
     for (int i = 0; i < input.size(); i++) {
-        if (mode == 0) {
+
+        // If no comments are in progress, wait for something to trigger a jump
+
+        if (mode == NO_COMMENTS_IN_PROGRESS) {
             if (input[i] == '/' && i+1 < input.size() && input[i+1] == '/') {
-                mode = 1;
+                mode = WAITING_FOR_NEWLINE;
                 i++;
                 continue;
             }
         
             if (input[i] == '/' && i+1 < input.size() && input[i+1] == '*') {
-                mode = 2;
+                mode = INSIDE_MULTILINE_COMMENT;
                 i++;
                 continue;
             }
 
             res += input[i];
         
-        } else if (mode == 1) {
+        } 
+
+        // If we've encountered a '//' comment, wait for a newline
+        
+        else if (mode == WAITING_FOR_NEWLINE) {
             if (input[i] == '\n') {
-                mode = 0;
+                mode = NO_COMMENTS_IN_PROGRESS;
             }
-        } else if (mode == 2) {
+        } 
+        
+        // If we've encountered a '/**/' comment, wait for the '*/'
+
+        else if (mode == INSIDE_MULTILINE_COMMENT) {
             if (input[i] == '*' && i+1 < input.size() && input[i+1] == '/') {
-                mode = 0;
+                mode = NO_COMMENTS_IN_PROGRESS;
                 i++;
             }
         }
@@ -64,16 +89,36 @@ std::string FixieTokenizer::stripComments(std::string input) {
     return res;
 }
 
+#undef NO_COMMENTS_IN_PROGRESS
+#undef WAITING_FOR_NEWLINE
+#undef INSIDE_MULTILINE_COMMENT
+
 /*
  * Tokenizes the code, assumed to have been cleaned of all comments
  */
 std::vector<std::string> *FixieTokenizer::chunk(std::string input) {
-    std::vector<std::string> *result = new std::vector<std::string>();
-    boost::xpressive::sregex re = boost::xpressive::sregex::compile("[0-9a-zA-Z]+|\"|\'|;|=|\\*|\\.|\\||\\|\\||&&|\\(|\\)|\\[|\\]|\\+|-|\\\\|\\/|\\*|\\?|\\$|\\^");
 
-    //Iterate over all the words in the input
+    //Create the vector to hold the result
+
+    std::vector<std::string> *result = new std::vector<std::string>();
+
+    //Build our massive regex to catch strings of letters and numbers, and all those
+    //important other symbols.
+    /*
+     
+       Here's the list of characters we match so far:
+
+        " ' ; = * . || && ( ) [ ] + - \ / ? $ ~
+
+     */
+
+    boost::xpressive::sregex re = boost::xpressive::sregex::compile("[0-9a-zA-Z]+|\"|\'|;|=|\\*|\\.||\\|\\||&&|\\(|\\)|\\[|\\]|\\+|-|\\\\|\\/|\\*|\\?|\\$|\\^");
+
+    //Iterate over all the matches in the input
 
     boost::xpressive::sregex_token_iterator begin( input.begin(), input.end(), re ), end;
+
+    //Spit the chunks into our vector
 
     for (; begin != end; ++begin) {
         result->push_back(*begin);
--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,44 @@
 
 
 int main(int argc, char **argv) {
+
+    //Remind the stupid that they must specify a file
     
     if (argc < 2) {
-        std::cout << "Must enter atleast one file to compile as an argument." << std::endl;
+        std::cout << "Must enter a file to compile as an argument." << std::endl;
         return 0;
     }
 
+    //Open up our file
+
     std::ifstream file(argv[1]);
     if (file.is_open()) {
+
+        //Provide some user feedback
+
         std::cout << "Cranking..." << std::endl;
-        FixieTokenizer *tok = new FixieTokenizer();
+
+        //Set up the tokenizer and our necessary variables
+
+        FixieTokenizer *tokenizer = new FixieTokenizer();
+        std::vector<FixieTokenizer::token> *tokens = new std::vector<FixieTokenizer::token>();
         std::string line;
         int lineNumber = 0;
-        std::vector<FixieTokenizer::token> *tokens = new std::vector<FixieTokenizer::token>();
+
+        //Read off the lines into the tokenizer
+
         while (file.good()) {
             lineNumber++;
             getline(file,line);
-            tok->tokenize(lineNumber,line,tokens);
+
+            //The tokenizer takes a vector as an argument, and 
+            //pumps its tokens into that vector
+
+            tokenizer->tokenize(lineNumber,line,tokens);
         }
+
+        //Here we just read off the result for testing
+
         for (int i = 0; i < tokens->size(); i++) {
             std::cout << tokens->at(i).lineNumber << ": " << tokens->at(i).string << std::endl;
         }
